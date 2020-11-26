148cc21cb81f13049887ea9c21715afa984f0181
liana alice <lyannalisse@gmail.com>
2020-11-25 21:02:36 -0300

    matéria dada em aula, após segunda aula

---
+++ javascript_aula1/1_operadores_console/script.js
@@ -0,0 +1,21 @@
+console.log('felix anivers Laura! Vc vai arrasar muito nessa vida guria! <3 ')
+
+
+// console.log é uma forma de mostrar para a desenvolvedora o código na nossa página. 
+let alunaProf = "kristhel"
+
+console.log("Boas tardes gurias! Eu sou o console que pousou em sua sopa." );
+console.log('agora, bora relembrar uns operadores matemáticos?');
+//soma
+console.log(10 + 12);
+//subtração 
+console.log('10 - 12= ',10 - 12);
+//multiplicação 
+console.log('10 * 3 = ',10 * 3);
+//divisão 
+console.log('10 / 3 = ',10 / 3);
+//expoente 
+console.log(4 ** 3)
+// resto -- quanto sobra de uma divisão
+//  simbolo porcentagem, modes 
+console.log(10 % 3)
